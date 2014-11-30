e92d061b48190f6ba7868c35212e17f9a28a8579
David Wickes <dave.wickes@gmail.com>
2014-11-30 17:29:38 +0000

    Adds package.json and a new test for initializing without data. And passes it.

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,21 +2,7 @@ githubUserSearch.controller('GitUserSearchController', function($scope, $resourc
 
     searchResource = $resource('https://api.github.com/search/users');
 
-    $scope.searchResult = {
-        "items": [{
-            "login": "antonydenyer",
-            "avatar_url": "https://avatars.githubusercontent.com/u/469160?v=3",
-            "html_url": "https://github.com/antonydenyer",
-        }, {
-            "login": "tansaku",
-            "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-            "html_url": "https://github.com/tansaku",
-        }, {
-            "login": "stephenlloyd",
-            "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-            "html_url": "https://github.com/stephenlloyd",
-        }]
-    }
+    //$scope.searchResult = {}
 
     $scope.doSearch = function() {
         $scope.searchResult = searchResource.get({
