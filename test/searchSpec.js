110c12ccd2174f2dc04973df7dc9a9a48787ec7c
David Wickes <dave.wickes@gmail.com>
2014-11-30 19:08:26 +0000

    Adds Gruntfile with Karma runner and a test task.

--- test/searchSpec.js
+++ test/searchSpec.js
@@ -1,7 +1,9 @@
 describe('GitUserSearchController', function() {
     beforeEach(module('GitUserSearch'));
 
-    var scope, ctrl;
+    var scope, ctrl, httpBackend;
+
+    var gitToken = '4ac1cd911baaff779f56c3d925210cfd1b354a50'
 
     beforeEach(inject(function($rootScope, $controller) {
         scope = $rootScope.$new();
@@ -10,16 +12,31 @@ describe('GitUserSearchController', function() {
         });
     }));
 
+
     it('should initialse with empty search result and term', function() {
         expect(scope.searchResult).toBeUndefined();
         expect(scope.searchTerm).toBeUndefined();
     });
 
-    it('displays search results when you search', function() {
-        scope.searchTerm = "gypsydave5";
-        scope.doSearch();
-        scope.$apply(); //forces Angular to resolve all pending promises. Kewl.
-        expect(scope.searchResult).toEqual([]);
+    describe('when searching for a user', function() {
+
+        beforeEach(inject(function($httpBackend) {
+            httpBackend = $httpBackend
+            httpBackend
+            .when("GET", 'https://api.github.com/search/users?access_token=4ac1cd911baaff779f56c3d925210cfd1b354a50&q=gypsydave5' )
+            .respond({
+                items: []
+            });
+        }));
+
+        it('displays search results when you search', function() {
+            scope.searchTerm = "gypsydave5";
+            scope.doSearch();
+            scope.$apply(); //forces Angular to resolve all pending promises. Kewl. Apart from HTTP requests. Lame
+            httpBackend.flush(); //this resolves the HTTP requests.
+            expect(scope.searchResult.items).toEqual([]);
+        });
+
     });
 
 });
